4f6c37db9cbea323dea4760b4d95be38737a26b7
dxlnr <daniel.illner@outlook.de>
2023-11-10 21:21:30 +0100

    args h fix

--- misc.py
+++ misc.py
@@ -4,6 +4,7 @@ import argparse
 def create_arg_parser():
     r"""Get arguments from command lines."""
     parser = argparse.ArgumentParser(description="Commandline Parser")
+    parser.add_argument("--h", type=str, help="help: Prints out help params.")
     parser.add_argument("--conf", type=str, help="Configuration file (path)")
 
     return parser
